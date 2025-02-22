453d4956656396da3e0b2a84123c0d1c989df20d
Kevin <kevin.cooray98@gmail.com>
2025-02-22 19:02:23 +0100

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from "@angular/router";
-import { ListAgentComponent } from "./components/agents/list-agent/list-agent.component";
-import { HomeComponent } from "./components/home/home.component";
-import { ListMapsComponent } from "./components/maps/list-maps/list-maps.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
-  { path: "list-agents", component: ListAgentComponent },
-  { path: "list-maps", component: ListMapsComponent }
+
 ];

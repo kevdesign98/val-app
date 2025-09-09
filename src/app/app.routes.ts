de30717a2290e551402feaf1ab9a31a3417afb3a
Kevin <kevin.cooray98@gmail.com>
2025-09-09 14:27:46 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { AgentsDetailsComponent } from "./components/agents-details/agents-detai
 import { MapStratComponent } from "./components/map-strat/map-strat.component";
 import { EsportsComponent } from "./components/esports/esports.component";
 import { WeaponsComponent } from "./pages/weapons/weapons.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "Home", pathMatch: "full" },
@@ -19,5 +22,8 @@ export const routes: Routes = [
   { path: "agents/:id", component: AgentsDetailsComponent },
   { path: 'maps-strat/:uuid', component: MapStratComponent },
   { path: '', redirectTo: 'maps', pathMatch: 'full' },
-  { path: '**', redirectTo: 'maps' }
+  { path: '**', redirectTo: 'maps' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: DashboardComponent }
 ];

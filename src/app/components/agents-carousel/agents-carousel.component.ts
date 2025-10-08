a6d8889991fa5ecf9d06b032c87c0ba522d7650e
kevdesign98 <kevin.cooray98@gmail.com>
2025-10-08 11:57:41 +0200

    updates

--- src/app/components/agents-carousel/agents-carousel.component.ts
+++ src/app/components/agents-carousel/agents-carousel.component.ts
@@ -34,8 +34,8 @@ export class AgentsCarouselComponent implements OnInit {
     });
   }
   responsiveOptions = [
-    { breakpoint: "1024px", numVisible: 3, numScroll: 1 },
-    { breakpoint: "768px", numVisible: 2, numScroll: 1 },
+    { breakpoint: "1024px", numVisible: 1, numScroll: 1 },
+    { breakpoint: "768px", numVisible: 1, numScroll: 1 },
     { breakpoint: "560px", numVisible: 1, numScroll: 1 },
   ];
 
--- src/app/components/agents/agents.component.ts
+++ src/app/components/agents/agents.component.ts
@@ -3,23 +3,27 @@ import { Component, OnInit } from "@angular/core";
 import { ListAgentsService } from "../../service/list-agents.service";
 import { RouterLink } from "@angular/router";
 import { NavbarComponent } from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
 //  import { Agents } from '../../models/agents';
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-agents",
   standalone: true,
-  imports: [CommonModule, RouterLink, NavbarComponent,NavbarComponent],
+  imports: [CommonModule, RouterLink, NavbarComponent, ButtonModule, FooterComponent],
   templateUrl: "./agents.component.html",
   styleUrl: "./agents.component.css",
 })
 export class AgentsComponent implements OnInit {
   agents: any[] = [];
+  filteredAgents: any[] = [];
+  roles: string[] = ["All", "Sentinel", "Initiator", "Controller", "Duelist"];
+  selectedRole: string = "All";
 
   constructor(private listAgentsService: ListAgentsService) {}
 
   ngOnInit() {
     this.listAgentsService.getAgents().subscribe((data) => {
-      // console.log("agenti", data);
       const sortedAgents = data.data.sort((a: any, b: any) =>
         a.displayName.localeCompare(b.displayName)
       );
@@ -34,6 +38,18 @@ export class AgentsComponent implements OnInit {
         imageName:
           agent.displayName.toLowerCase().replace(/[^a-z0-9]/g, "") + ".png",
       }));
+
+      this.filteredAgents = this.agents;
     });
+  }
+  filterByRole(role: string) {
+    this.selectedRole = role;
+    if (role === "All") {
+      this.filteredAgents = this.agents;
+    } else {
+      this.filteredAgents = this.agents.filter(
+        (agent) => agent.role?.displayName?.toLowerCase() === role.toLowerCase()
+      );
+    }
   }
 }

29576010435c8a568e46465da8d1f385430c6edf
kevdesign98 <kevin.cooray98@gmail.com>
2026-04-03 21:13:27 +0200

    fix: total sync of environment naming and vercel keys

--- set-env.js
+++ set-env.js
@@ -9,9 +9,11 @@ const envConfigFile = `export const environment = {
   apiUrl: 'https://api.henrikdev.com/valorant'
 };`;
 
-const envDir = path.join(__dirname, 'src', 'environments');
+const envDir = path.join(__dirname, 'src', 'environment');
+const targetPath = path.join(envDir, 'environment.ts');
+const prodPath = path.join(envDir, 'environment.prod.ts');
 if (!fs.existsSync(envDir)) { fs.mkdirSync(envDir, { recursive: true }); }
 
-fs.writeFileSync(path.join(envDir, 'environment.ts'), envConfigFile);
-fs.writeFileSync(path.join(envDir, 'environment.prod.ts'), envConfigFile);
+fs.writeFileSync(targetPath, envConfigFile);
+fs.writeFileSync(prodPath, envConfigFile);
 console.log('✅ Environment pronto!');
